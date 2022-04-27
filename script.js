72faf727a5743a0f64e9bb8c37584116f115c404
EricPortela <ericq.portela@gmail.com>
2022-04-27 14:17:44 +0200

    About us

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Javascript for opening and closing of pop-up menu!
+
+function openMenu() {
+    document.getElementById("pop_over").style.width = "100%";
+} 
+
+function closeMenu() {
+    document.getElementById("pop_over").style.width = "0%";
+} 
